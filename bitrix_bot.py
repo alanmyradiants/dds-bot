81a1a7f0b6d81b4df6bb120d7cb1570316417252
Alan <alan.myradiants@gmail.com>
2026-04-13 23:38:09 +0300

    Update bitrix_bot.py

--- bitrix_bot.py
+++ bitrix_bot.py
@@ -233,4 +233,4 @@ def health():
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=True)м
